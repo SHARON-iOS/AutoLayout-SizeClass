7cfcfd4b9d3a6bee35f0052cca3b2fa6499fbd48
SHARON-iOS <sharon@sparkouttech.com>
2020-09-02 18:15:04 +0530

    Autolayout with size class concept

---
+++ AutoLayout demo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AutoLayout demo
+//
+//  Created by SHARON D ROSE on 29/08/20.
+//  Copyright © 2020 SHARON D ROSE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+
+    }
+
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    
+}
+
